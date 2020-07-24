c4118299fb97d9af3f22f76f4ca8b2d58512250e
ryohei1114 <64072936+ryohei1114@users.noreply.github.com>
2020-07-25 01:08:49 +0900

    s3

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,7 +3,7 @@ require "refile/s3"
 aws = {
   access_key_id: "AWS_ACCESS_KEY_ID",
   secret_access_key: "AWS_SECRET_ACCESS_KEY",
-  region: "ap-northeast-1",
+  region: "ap-northeast-1", 
   bucket: "ownquestion-bucket",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
